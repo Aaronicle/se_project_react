9e727a952621ed7447149947d519a994b08f3277
Aaronicle <aaroncolorguy@gmail.com>
2025-01-25 09:30:54 -0500

    weather card added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import "./Header.css";
+import logo from "../../assets/Logo.svg";
+import avatar from "../../assets/Avatar.png";
+
+function Header() {
+  return (
+    <>
+      <header className="header">
+        <img src={logo} alt="" className="header__logo" />
+        <p className="header__date-and-location">DATE, LOCATION</p>
+        <button className="header__add-clothes-btn">+Add clothes</button>
+        <div className="header__user-container">
+          <p className="header__username">Terrence Tegegne</p>
+          <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
